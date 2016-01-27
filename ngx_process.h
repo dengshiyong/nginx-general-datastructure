3b13b112d48eec19eb774874c915bb7638fc539f
= <=>
2016-01-27 23:52:48 +0800

    feat:added ngx_pool memory pool related function feat:added ngx_log log related function

--- ngx_process.h
+++
@@ -1,6 +0,0 @@
-#ifndef NGX_PROCESS_H
-#define NGX_PROCESS_H
-#include "ngx_core.h"
-typedef pid_t ngx_pid_t;
-#endif // NGX_PROCESS_H
-
--- ngx_rbtree.h
+++
@@ -1,8 +0,0 @@
-#ifndef NGX_RBTREE_H
-#define NGX_RBTREE_H
-#include "ngx_core.h"
-
-typedef ngx_uint_t ngx_rbtree_key_t;
-typedef ngx_int_t ngx_rbtree_key_int_t;
-#endif // NGX_RBTREE_H
-
