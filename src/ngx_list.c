cb572f063734161eb0e7471bf836d828d5e1d966
iamdsy <iamdsy1@126.com>
2016-02-02 11:38:27 +0800

    feature: add the ngx_list datastructure,but do not do the test

--- src/ngx_list.c
+++ src/ngx_list.c
@@ -15,23 +15,26 @@ ngx_list_t *ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size){
 
 
 void *ngx_list_push(ngx_list_t *list){
+    void *elts;
     ngx_list_part_t *lp;
-    if (list->nalloc > list->last->nelts){
-        list->last->nelts ++;
-        return (list->last->elts + list->size);
+    lp = list->last;
+    if (list->nalloc == list->last->nelts){
+        lp = ngx_palloc(list->pool,sizeof(ngx_list_part_t));
+        if (lp == NULL){
+            return NULL;
+        }
+        lp->elts = ngx_palloc(list->pool,list->nalloc * list->size);
+        if (lp->elts == NULL){
+            return NULL;
+        }
+        lp->next = NULL;
+        lp->nelts = 0;
+        list->last->next = lp;
+        list->last = lp;
     }
-    lp = ngx_pcalloc(list->pool,sizeof(ngx_list_part_t));
-    if (lp == NULL){
-        return NULL;
-    }
-    lp->elts = ngx_pcalloc(list->pool,list->nalloc * list->size);
-    if (lp->elts == NULL){
-        return NULL;
-    }
-    lp->next = NULL;
-    lp->nelts = 0;
-    list->last->next = lp;
-    return lp->elts;
+    elts = (char *)lp->elts + list->size * lp->nelts;
+    lp->nelts ++;
+    return elts;
 }
 
 
