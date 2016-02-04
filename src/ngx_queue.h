e39868c70047a6c01489a03f4c917ac5963823af
dengshiyong <iamdsy1@126.com>
2016-02-04 14:34:38 +0800

    media-status

--- src/ngx_queue.h
+++ src/ngx_queue.h
@@ -27,6 +27,42 @@ struct ngx_queue_s {
     (x)->prev->next = x;               \
     (h)->prev = x
 
+#define ngx_queue_head(h)               \
+    (h)->next
+
+#define ngx_queue_last(h)               \
+    (h)->prev
+
+#define ngx_queue_sentinel(h)           \
+    (h)
+
+
+#if(NGX_DEBUG)
+#define ngx_queue_remove(x)           \
+    (x)->prev->next = (x)->next;         \
+    (x)->next->prev = (x)->prev;        \
+    (x)->prev = NULL;                   \
+    (x)->next = NULL
+#else
+#define ngx_queue_remove(x)           \
+    (x)->prev->next = (x)->next;         \
+    (x)->next->prev = (x)->prev
+
+#endif
+
+
+#define ngx_queue_split(h,q,n)                \
+    (n)->prev = (h)->prev;                    \
+    (n)->prev->next = n;                      \
+    (n)->next = q;                            \
+    (h)->prev = (q)->prev;                    \
+    (q)->prev->next = h;                      \
+    (q)->prev = n
+
+
+
+
+
 
 #endif // NGX_QUEUE_H
 
